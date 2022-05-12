8db7b5f9dc799d31f2f18134e42c0a17ac9f84d0
Vinicius Pereira <Vinicius.Pereira@processor.com.br>
2022-05-12 08:19:17 -0300

    ajustes a mais

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,8 @@
 import express from "express";
 import { routes } from "./routes";
 import cors from "cors";
+import * as dotenv from "dotenv";
+dotenv.config({})
 
 const app = express();
 
@@ -11,4 +13,4 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use("/api", routes);
 
-app.listen(process.env.PORT || 3333, () => console.log('HTTP Status Running API'));
+app.listen(process.env.PORT|| 3333, () => console.log('HTTP Status Running API'));
